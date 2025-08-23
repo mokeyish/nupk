9b084cbe70a4e357f8ae80ba39985308d0bdd51a
YISH <mokeyish@hotmail.com>
2025-08-23 21:37:22 +0800

    Improve shell version comparison logic

--- install.sh
+++ install.sh
@@ -8,9 +8,28 @@ INSATLL_DIR="$1"
 INSATLL_DIR=${INSATLL_DIR:=$HOME/.nupk}
 PREFIX=${PREFIX:=$HOME/.local}
 
-ver_gte()
-{
-    printf '%s\n%s\n' "$2" "$1" | sort --check=quiet --version-sort
+ver_gte() {
+    ver_arr() {
+        IFS='.' read -r -a ver <<< "$1"
+        echo "${ver[@]}"
+    }
+
+    local curr=($(ver_arr "$1"))
+    local req=($(ver_arr "$2"))
+
+    for i in 0 1 2; do
+        curr_val=${curr[i]:-0}
+        req_val=${req[i]:-0}
+
+        if (( curr_val > req_val )); then
+            return 0
+        elif (( curr_val < req_val )); then
+            return 1
+        fi
+    done
+
+    # 版本号相等
+    return 1
 }
 
 echo "Cloning nupk to $INSATLL_DIR..."
@@ -19,12 +38,13 @@ git clone --depth 1 --no-checkout https://github.com/mokeyish/nupk.git $INSATLL_
 
 cd $INSATLL_DIR
 
-if ver_gte $(git --version) 2.25.0;then
+currrent_git_version=$(git --version | awk '{print $3}')
+required_git_version="2.25.0"
+if ver_gte "$currrent_git_version" "$required_git_version";then
     git config core.sparseCheckout true
-
     git sparse-checkout set --no-cone  '/*' '!/**/tests/' '!/install.sh'
 else
-    echo "Warning: Sparse checkout is not supported in your git version, please upgrade git to 2.25.0 or later."
+    echo "Warning: Sparse checkout is not supported in your git version, please upgrade git to $required_git_version or later."
 fi
 
 
